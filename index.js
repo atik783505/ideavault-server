668634305648dbbb93c8172a535cdc0e5770844a
atik783505 <atik13672@gmail.com>
2026-05-19 22:18:14 +0600

    comment post opration complete

--- index.js
+++ index.js
@@ -40,22 +40,28 @@ async function run() {
             const result = await ideas.findOne({ _id: new ObjectId(id) })
             res.send(result)
         })
-        app.get('/myideas/:userId', async(req,res) => {
-            const {userId} = req.params
-            const result = await ideas.find({userId}).toArray()
+        app.get('/myideas/:userId', async (req, res) => {
+            const { userId } = req.params
+            const result = await ideas.find({ userId }).toArray()
             res.send(result)
         })
-        app.delete('/myideas/:id', async(req,res) => {
-            const {id} = req.params
-            const result = await ideas.deleteOne({_id : new ObjectId(id)})
+        app.delete('/myideas/:id', async (req, res) => {
+            const { id } = req.params
+            const result = await ideas.deleteOne({ _id: new ObjectId(id) })
             res.send(result)
         })
-        app.patch('/myideas/:id', async(req,res) => {
-            const {id} = req.params
+        app.patch('/myideas/:id', async (req, res) => {
+            const { id } = req.params
             const updateData = req.body
             const result = await ideas.updateOne(
-                {_id: new ObjectId(id)},
-                {$set:updateData})
+                { _id: new ObjectId(id) },
+                { $set: updateData })
+            res.send(result)
+        })
+        app.post('/comment', async (req, res) => {
+            const comment = req.body
+            comment.createdAt = new Date();
+            const result = await commentCollection.insertOne(comment)
             res.send(result)
         })
         await client.db("admin").command({ ping: 1 });
